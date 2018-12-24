90625a3cca580406df05196e8fce7c92f5f97f19
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2018-12-24 17:08:27 -0300

    data.js: se incorpora funcion BMI y Data para sortData es arreglo derivado de funcion BMI. main.js: la data deriva del arreglo BMI y se incorpora BMI en la tabla y propiedades. index.html: se agrega boton weight y BMI

--- src/data.js
+++ src/data.js
@@ -1,22 +1,27 @@
-function sortData(data, sortBy, sortOrder){
-  /* las 2 variables y el for se declaran para acceder al arreglo de objetos con las caracteristicas de cada pokemón */
-  let arr = [];
-  let value = Object.values(data)[0];
-  for (let i = 0; i < value.length; i++){
-    arr.push(value[i]);
+function bmiInData(data){
+  let arrModified = Object.values(data)[0];
+  for (let i = 0; i < arrModified.length; i++){
+      arrModified[i].weight = parseFloat(arrModified[i].weight);
+      arrModified[i].height = parseFloat(arrModified[i].height);
+      arrModified[i].BMI = parseFloat((arrModified[i].weight/(arrModified[i].height ** 2)).toFixed(2));
   }
+  return arrModified;
+}
+
+
+function sortData(data, sortBy, sortOrder){
   /* se utiliza metodo sort() aprendido aquí https://www.w3schools.com/js/js_array_sort.asp para números y strings */
   /* lo siguiente ordena según valor numérico, dividiendose en ascendente(true) y descendente(false) */
-  if (typeof arr[0][sortBy] === "number") {
+  if (typeof data[0][sortBy] === "number") {
     if (sortOrder === true){
-      return arr.sort(function(a, b){return a[sortBy] - b[sortBy]});
+      return data.sort(function(a, b){return a[sortBy] - b[sortBy]});
     } else if (sortOrder == false){
-      return arr.sort(function(a, b){return b[sortBy] - a[sortBy]});
+      return data.sort(function(a, b){return b[sortBy] - a[sortBy]});
     } 
   /* lo siguiente ordena según valor string, dividiendose en ascendente(true) y descendente(false) */
-  } else if (typeof arr[0][sortBy] === "string") {    
+  } else if (typeof data[0][sortBy] === "string") {    
     if (sortOrder === true){
-      return arr.sort(function(a, b){ 
+      return data.sort(function(a, b){ 
         var x = a[sortBy].toLowerCase();
         var y = b[sortBy].toLowerCase();       
         if (x < y) {
@@ -28,7 +33,7 @@ function sortData(data, sortBy, sortOrder){
         }
       });
     } else if (sortOrder == false){
-      return arr.sort(function(a, b){ 
+      return data.sort(function(a, b){ 
         var x = a[sortBy].toLowerCase();
         var y = b[sortBy].toLowerCase();       
         if (x < y) {
