2fc4901b87ed9e528b615970e68b340f884c6c82
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2019-01-10 22:42:58 -0300

    main.js: se quitan algunos comentarios

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+/* al apretar en el logo se refresca la página */
+document.getElementById("pokemon-go").addEventListener("click", function(){
+    location.reload();
+})
+
 let table = document.getElementById("myTable"); 
 /* table es la tabla inicial que despliega toda la Data, 
 aquí se conecta su espacio con el html */
@@ -52,60 +57,55 @@ function refreshButtons(buttonArray){
     }
 }
 
-/* al apretar en el logo se refresca la página */
-document.getElementById("pokemon-go").addEventListener("click", function(){
-    location.reload();
-})
-
 /* se llama a refrescar los botones de ordenado y filtrado utilizando la función anterior*/
 refreshButtons(arrTypes);
 refreshButtons(arrProperties);
 
 /* función que llama a sortData.js(en data.js) cuando se apreten los botones de ordenado */
 function displaySorting(sortBy,objectArray){
-    const sortButton = document.getElementById(sortBy).style; //se define constante para guardar estilo de botón  
-    let counter = 0;//contador que sirve para orden ascendente o descentente(par/impar) 
-    document.getElementById(sortBy).addEventListener("click", function(){ //si se apreta el botón que esta tomando el valor de "sortBy"...
-        let sortOrder = true; //el orden por defecto es ascendente(true)
-        if (counter % 2 === 0){ //si el contador es par
-            refreshButtons(arrProperties); //refrescar botones
-            sortButton.background = "lightblue"; //cambiar botones a color azul 
-            counter += 1 // sumar 1 para que la siguiente vez que se aprete el boton ordene en forma descendente    
-            return arrayToTable(window.processData.sortData(objectArray, sortBy, sortOrder), table); //llama a sortData para que ordene la tabla segun el boton apretado en orden ascendente            
-        } else { //si el contador es impar
-            counter += 1 // sumar 1 para que la siguiente vez que se aprete el botón ordene en forma ascendente 
-            refreshButtons(arrProperties);//refrescar botones
-            sortButton.background = "lightblue"; //cambiar botones a color verde 
-            sortOrder = false; //se reasigna sortOrder para que se realice orden descendente         
-            return arrayToTable(window.processData.sortData(objectArray, sortBy, sortOrder), table); //llama a sortData para que se muestre la tabla ordenada segun el boton apretado en orden descendente                     
+    const sortButton = document.getElementById(sortBy).style;   
+    let counter = 0; 
+    document.getElementById(sortBy).addEventListener("click", function(){ 
+        let sortOrder = true; 
+        if (counter % 2 === 0){ 
+            refreshButtons(arrProperties); 
+            sortButton.background = "lightblue";  
+            counter += 1    
+            return arrayToTable(window.processData.sortData(objectArray, sortBy, sortOrder), table);             
+        } else { 
+            counter += 1  
+            refreshButtons(arrProperties);
+            sortButton.background = "lightblue";  
+            sortOrder = false;         
+            return arrayToTable(window.processData.sortData(objectArray, sortBy, sortOrder), table);                     
         }                  
     })  
 }
 
 /* función que llama a filterData.js(en data.js) cuando se apreten los botones de filtrado */
 function displayFilter(condition,objectArray){
-    const filteredData = window.processData.filterData(objectArray, condition); //se define constante para guardar data filtrada según condición(boton de filtro)
-    const filterButton = document.getElementById(condition).style; //se define constante para guardar estilo de botón
-    let percent = document.getElementById("percent");// se define variable para acceder a elemento porcentaje
-    document.getElementById(condition).addEventListener("click", function(){ //si se apreta el botón que esta tomando el valor de "condition"...
-        if (filterButton.background === "white"){ //si el botón es blanco         
-            refreshButtons(arrTypes); //refrescar todos los botones de filtrado
-            refreshButtons(arrProperties); //refrescar todos los botones de ordenado            
-            filterButton.background = "lightgrey"; //cambiar boton seleccionado a color gris 
-            percent.style.display = "block";//aparece caja de porcentaje
+    const filteredData = window.processData.filterData(objectArray, condition); 
+    const filterButton = document.getElementById(condition).style; 
+    let percent = document.getElementById("percent");
+    document.getElementById(condition).addEventListener("click", function(){ 
+        if (filterButton.background === "white"){          
+            refreshButtons(arrTypes); 
+            refreshButtons(arrProperties);             
+            filterButton.background = "lightgrey"; 
+            percent.style.display = "block";
             percent.innerHTML = window.processData.percentageFilteredData(filteredData, data) + "% de los pokemones son de tipo " + condition + ".";                      
-            iterate(displaySorting, arrProperties, filteredData);//llamar a display sorting para que se pueda hacer ordenado dentro de filtrado, en caso que se aprete un boton de sorting mientras esté apretado un botón de filter                      
-            return arrayToTable(filteredData, table);//mostrar la data filtrada segun el botón apretado                                     
-        } else { //si el botón no es blanco (es gris)   
-            percent.style.display = "none";//desaparece caja de porcentaje        
-            filterButton.background = "white";//cambiar boton seleccionado a color blanco (representa descliqueado)            
-            refreshButtons(arrProperties);//refrescar todos los botones de ordenado
-            iterate(displaySorting, arrProperties, data);//llamar a display sorting para que se pueda volver a ordenar la data original una vez descliqueado
-            return arrayToTable(data, table);//mostrar la data no filtrada. 
+            iterate(displaySorting, arrProperties, filteredData);                     
+            return arrayToTable(filteredData, table);                                     
+        } else {   
+            percent.style.display = "none";       
+            filterButton.background = "white";           
+            refreshButtons(arrProperties);
+            iterate(displaySorting, arrProperties, data); 
+            return arrayToTable(data, table);
         }                  
     })      
 } 
 
-iterate(displaySorting, arrProperties, data);//para pedir que se ejecute la función que llama a displaySorting con todos los posibles valores de sortBy(botones de ordenado)
-iterate(displayFilter, arrTypes, data); //para pedir que se ejecute la función que llama a displayFilter con todos los posibles valores de condition(botones de filtrado)
+iterate(displaySorting, arrProperties, data);
+iterate(displayFilter, arrTypes, data); 
  
